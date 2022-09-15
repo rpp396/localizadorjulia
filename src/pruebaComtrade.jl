6ae9ecf532211e427a1f180aca8cb0a3bca300c4
RPP <rperdomo396@gmail.com>
2022-09-14 22:48:21 -0300

    Ver DF y lista de cosas a hacer

--- src/pruebaComtrade.jl
+++ src/pruebaComtrade.jl
@@ -1,4 +1,9 @@
 using COMTRADE
+using DataFrames
+
 
 
 z1 = read_comtrade("./Oscilos/02.09.22 22.09.55.000.000")
+VSCodeServer.vscodedisplay(z1.cfg.A) #Para ver los DataFrames
+VSCodeServer.vscodedisplay(z1.cfg.D) #Para ver los DataFrames
+VSCodeServer.vscodedisplay(z1.dat) #Para ver los DataFrames

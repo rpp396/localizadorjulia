5f7caec95c68a2fe1ff182d517a84d3ee1596fb8
RPP <rperdomo396@gmail.com>
2022-09-30 14:31:53 -0300

    Cambios en Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,8 @@
 #= push!(LOAD_PATH,"../src/")
 push!(LOAD_PATH,"../../src/") =#
-include("../src/pruebaComtrade.jl")
-push!(LOAD_PATH,"../src/")
-using Documenter, pruebaComtrade
 
-stmakedocs(sitename="Prueba Documenter")
+push!(LOAD_PATH,"../../src/")
+using Documenter, pruebaComtrade
+include("../src/pruebaComtrade.jl")
+makedocs(sitename="Prueba Documenter")
+
--- src/pruebaComtrade.jl
+++ src/pruebaComtrade.jl
@@ -1,10 +1,15 @@
 module pruebaComtrade
-
+"""
+DocString de pruebaDocString
+"""
+function pruebaDocString()
+    1 + 1
+end
+pruebaDocString()
 using COMTRADE
 using DataFrames
-
 z1 = read_comtrade("./Oscilos/02.09.22 22.09.55.000.000")
-VSCodeServer.vscodedisplay(z1.cfg.A) #Para ver los DataFrames
+#= VSCodeServer.vscodedisplay(z1.cfg.A) #Para ver los DataFrames
 VSCodeServer.vscodedisplay(z1.cfg.D) #Para ver los DataFrames
-VSCodeServer.vscodedisplay(z1.dat) #Para ver los DataFramesst
+VSCodeServer.vscodedisplay(z1.dat) #Para ver los DataFramesst =#
 end

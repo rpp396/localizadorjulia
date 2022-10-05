b7ff88b0349502b0783c7e95fa7de75fbe3b717c
DGM1971 <dgm1971@gmail.com>
2022-10-04 22:51:00 -0300

    dgm

--- src/pruebaComtrade.jl
+++ src/pruebaComtrade.jl
@@ -8,7 +8,7 @@ end
 pruebaDocString()
 using COMTRADE
 using DataFrames
-z1 = read_comtrade("./Oscilos/02.09.22 22.09.55.000.000")
+z1 = read_comtrade("./Oscilos/NOR-T2_osc1")
 #= VSCodeServer.vscodedisplay(z1.cfg.A) #Para ver los DataFrames
 VSCodeServer.vscodedisplay(z1.cfg.D) #Para ver los DataFrames
 VSCodeServer.vscodedisplay(z1.dat) #Para ver los DataFramesst =#
